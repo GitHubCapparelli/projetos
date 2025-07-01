90021d3b9433395b37b88a52681a2940b551d108
Capparelli <kdcapparelli@hotmail.com>
2025-07-01 13:58:40 -0300

    a

--- docs/.shared/assets/js/core.js
+++ docs/.shared/assets/js/core.js
@@ -4,13 +4,16 @@ function toogleMenu() {
   sideExpanded = !sideExpanded;
   const btn = document.getElementById('btnToogle');
   const bar = document.getElementById('sidebar');
+  const bdy = document.getElementById('sideBody');
   if (btn && bar) {
     if (sideExpanded) {
-      bar.style.width = '200px';
-      btn.className = 'menu-x fa-solid fa-x';
+      bar.style.width   = '200px';
+      btn.className     = 'menu-x fa-solid fa-x';
+      bdy.style.display = 'block';
     } else {
-      bar.style.width = '25px';
-      btn.className = 'menu-bars fa-solid fa-bars';
+      bar.style.width   = '35px';
+      btn.className     = 'menu-bars fa-solid fa-bars';
+      bdy.style.display = 'none';
     }
   }
 }
