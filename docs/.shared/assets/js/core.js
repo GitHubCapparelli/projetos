65dd14e0cb395a9e02ee71dffbbdb4b17258350a
Capparelli <kdcapparelli@hotmail.com>
2025-07-04 13:45:31 -0300

    d

--- docs/.shared/assets/js/core.js
+++ docs/.shared/assets/js/core.js
@@ -112,7 +112,7 @@ window.addEventListener('scroll', () => {
   const header = document.getElementById('stickyHeader');
   const bannerHeight = document.querySelector('.main-banner').offsetHeight;
 
-  if (window.scrollY > bannerHeight) {
+  if (window.scrollY > 100 ) { //bannerHeight) {
     header.classList.add('show');
   } else {
     header.classList.remove('show');
