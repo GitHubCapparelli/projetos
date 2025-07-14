13ef469d443f754ef08f1750d2f8909ccbbcb0f9
Capparelli <kdcapparelli@hotmail.com>
2025-07-14 08:09:51 -0300

    f

--- docs/.shared/assets/js/pageContext.js
+++ docs/.shared/assets/js/pageContext.js
@@ -81,7 +81,7 @@ const menuHTML = {
 <ul class="treeview list-unstyled ps-2">
   <li>
     <span class="tree-toggle" onclick="toggleNode(this)">
-      <i class="fas fa-folder me-1"></i>Dist. Territorial Cad. Único
+      <i class="fas fa-folder me-1"></i>Dist. Territorial - Cadastro Único
     </span>
     <ul class="nested list-unstyled ps-3 d-none">
       <li><a href="#"><i class="fas fa-file-alt me-1"></i>Apresentação</li>
