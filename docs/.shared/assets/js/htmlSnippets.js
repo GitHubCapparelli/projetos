d6434cafd2297d310e15584471309bfd02d59f4a
Capparelli <kdcapparelli@hotmail.com>
2025-07-17 16:15:43 -0300

    d

--- docs/.shared/assets/js/htmlSnippets.js
+++ docs/.shared/assets/js/htmlSnippets.js
@@ -58,7 +58,7 @@ export const paifUserMenu = `<ul class="treeview list-unstyled p-0 m-0">
       <i class="fas fa-folder me-2"></i>Mais...
     </span>
     <ul class="nested list-unstyled ps-4 d-none">
-      <li><a href="/projetos/tech/"><i class="fas fa-file-alt me-2"></i>Manual técnico</a></li>
+      <li><a href="/projetos/paif/tech/"><i class="fas fa-file-alt me-2"></i>Manual técnico</a></li>
       <li><a href="/projetos/"><i class="fas fa-file-alt me-2"></i>Outras soluções GERVIS</a></li>
     </ul>
   </li>
@@ -138,7 +138,7 @@ export const paifTechMenu = `<ul class="treeview list-unstyled p-0 m-0">
       <i class="fas fa-folder me-2"></i>Mais...
     </span>
     <ul id="folderMais" class="nested list-unstyled ps-4 d-none">
-      <li><a href="/projetos/user/"><i class="fas fa-file-alt me-2"></i>Manual do usuário</a></li>
+      <li><a href="/projetos/paif/user/"><i class="fas fa-file-alt me-2"></i>Manual do usuário</a></li>
       <li><a href="/projetos/"><i class="fas fa-file-alt me-2"></i>Outras soluções GERVIS</a></li>
     </ul>
   </li>
