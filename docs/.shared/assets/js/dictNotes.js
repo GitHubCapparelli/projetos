82ab63538113b652a59b4f3db2c061b659ee536b
Capparelli <kdcapparelli@hotmail.com>
2025-07-18 14:27:58 -0300

    d

--- docs/.shared/assets/js/dictNotes.js
+++ docs/.shared/assets/js/dictNotes.js
@@ -5,7 +5,7 @@ export const noteTitle = {
 
 export const noteHTML = {
   subsasUnidadesGestao: `<div class="d-flex flex-column justify-content-between h-100"><div><p class="mt-0 mb-0">A <b>SUBSAS</b> (Subsecretaria de Assistência Social) conta atualmente* com as seguintes unidades de gestão:</p>
-  <ul>
+  <ul class="ml-0">
     <li><b>CPSB</b> Coordenação de Proteção Social Básica</li>
     <li><b>CPSM</b> Coordenação de Proteção Social Especial de Média Complexidade</li>
     <li><b>CPSB</b> Coordenação de Proteção Social Básica</li>
@@ -19,11 +19,11 @@ export const noteHTML = {
 
   sedesOrgaos: `<div class="d-flex flex-column justify-content-between h-100"><div><p class="mt-0 mb-0">O gabinete da <b>SEDES</b> (Secretaria de Estado de Desenvolvimento Social) do <b>GDF</b> conta atualmente* com as seguintes unidades administrativas:</p>
   <ul>
-    <li><b>SEEDS</b> Subsecretaria de Administração Geral</li>
-    <li><b>SUAG</b> Coordenação de Proteção Social Básica</li>
-    <li><b>SUGIP</b> Subsecretaria de Governança, Inovação e Educação Permanente</li>
-    <li><b>SUBSAN</b> Subsecretaria de Segurança Alimentar e Nutricional </li>
-    <li><b>SUBSAS</b> Subsecretaria de Assistência Social </li>
+    <li class="ml-0"><b>SEEDS</b> Subsecretaria de Administração Geral</li>
+    <li class="ml-0"><b>SUAG</b> Coordenação de Proteção Social Básica</li>
+    <li class="ml-0"><b>SUGIP</b> Subsecretaria de Governança, Inovação e Educação Permanente</li>
+    <li class="ml-0"><b>SUBSAN</b> Subsecretaria de Segurança Alimentar e Nutricional </li>
+    <li class="ml-0"><b>SUBSAS</b> Subsecretaria de Assistência Social </li>
   </ul>
   </div><div><p class="mt-0 mb-0">* Agosto 2025</p><p class="mt-0 mb-0">Mais em <a href="#" target="_blank">link</a></p></div></div>`
 };
