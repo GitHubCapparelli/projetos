4e1e2376f8df385ffef6903c9d6f34bf5179aed1
Capparelli <kdcapparelli@hotmail.com>
2025-07-18 13:45:22 -0300

    d

--- docs/.shared/assets/js/dictNotes.js
+++ docs/.shared/assets/js/dictNotes.js
@@ -1,9 +1,30 @@
 export const noteTitle = {
-  test: 'Teste',
-  test2: 'Teste 2'
+  subsas-unidadesGestao: 'SUBSAS - Unidades funcionais',
+  sedes-orgaos: 'SEDES - Órgãos'
 };
 
 export const noteHTML = {
-  test: `<h1>Teste</h1>`,
-  test2: `<h1>Teste 2</h1>`
+  subsas-unidadesGestao: `<div class="d-flex flex-column justify-content-between"><div><p class="mt-0 mb-0">A <b>SUBSAS</b> (Subsecretaria de Assistência Social) conta atualmente* com as seguintes unidades de gestão:</p>
+  <ul>
+    <li><b>CPSB</b> Coordenação de Proteção Social Básica</li>
+    <li><b>CPSM</b> Coordenação de Proteção Social Especial de Média Complexidade</li>
+    <li><b>CPSB</b> Coordenação de Proteção Social Básica</li>
+    <li><b>CPSA</b> Coordenação de Proteção Social Especial de Alta Complexidade</li>
+    <li><b>CTRAR</b> Coordenação de Gestão de Transferência de Renda e Cadastro Único</li>
+    <li><b>UNIBS</b> Unidade de Benefícios Socioassistenciais</li>
+    <li><b>UNIPAR</b> Unidade de Parcerias com as OSC [Organização da Sociedade civil, terceirização de serviços]</li>
+    <li><b>GERVIS</b> Gerência de Vigilância Socioassistencial</li>
+  </ul>
+  </div><div><p class="mt-0 mb-0">* Agosto 2025</p><p class="mt-0 mb-0">Mais em <a href="#" target="_blank">link</a></p></div></div>`,
+
+  sedes-orgaos: `<div class="d-flex flex-column justify-content-between"><div><p class="mt-0 mb-0">A <b>SEDES</b> (Secretaria de Estado de Desenvolvimento Social) do <b>GDF</b> conta atualmente* com as seguintes unidades administrativas:</p>
+  <ul>
+    <li><b>Gabinete</b></li>
+    <li><b>SEEDS</b> Subsecretaria de Administração Geral</li>
+    <li><b>SUAG</b> Coordenação de Proteção Social Básica</li>
+    <li><b>SUGIP</b> Subsecretaria de Governança, Inovação e Educação Permanente</li>
+    <li><b>SUBSAN</b> Subsecretaria de Segurança Alimentar e Nutricional </li>
+    <li><b>SUBSAS</b> Subsecretaria de Assistência Social </li>
+  </ul>
+  </div><div><p class="mt-0 mb-0">* Agosto 2025</p><p class="mt-0 mb-0">Mais em <a href="#" target="_blank">link</a></p></div></div>`
 };
