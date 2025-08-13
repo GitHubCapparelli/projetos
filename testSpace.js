3be318cea41050e135fd709ed273fe3b8c480761
Capparelli <kdcapparelli@hotmail.com>
2025-08-13 10:22:01 -0300

    d

--- testSpace.js
+++ testSpace.js
@@ -1,1 +1,28 @@
+{
+    "type": "object",
+        "properties"        : {
+        "Id"                : { "type"  : ["integer", "null"] },
+        "Unidade"           : { "oneOf" : [  {"type": "null" }, {"type": "object", "properties": { 
+                                "Id"    : { "type": "integer" } }, "required": ["Id"] } ] },
+        "Inicio"            : { "type"  : ["string", "null"],   "format": "date" },
+        "Fim"               : { "type"  : ["string", "null"],   "format": "date" },
+        "Pobreza"           : { "type"  : ["boolean", "null"] },
+        "PBF"               : { "type"  : ["boolean", "null"] },
+        "DescumprePBF"      : { "type"  : ["boolean", "null"] },
+        "BPC"               : { "type"  : ["boolean", "null"] },
+        "TrabalhoInfantil"  : { "type"  : ["boolean", "null"] },
+        "EmAcolhimento"     : { "type"  : ["boolean", "null"] },
+        "Beneficiario"      : { "oneOf" : [  {"type": "null" }, {"type": "object", "properties": { 
+                                "Id"    : { "type": "integer" } }, "required": ["Id"] } ] },
+        "Especialista"      : { "oneOf" : [  {"type": "null" }, {"type": "object", "properties": { 
+                                "Id"    : { "type": "integer" } }, "required": ["Id"] } ] },
+        "InseridoPor"       : { "oneOf" : [  {"type": "null" }, {"type": "object", "properties": { 
+                                "Id"    : { "type": "integer" } }, "required": ["Id"] } ] },
+        "AtualizadoPor"     : { "oneOf" : [  {"type": "null" }, {"type": "object", "properties": { 
+                                "Id"    : { "type": "integer" } }, "required": ["Id"] } ] },
+        "EncerradoPor"      : { "oneOf" : [  {"type": "null" }, {"type": "object", "properties": { 
+                                "Id"    : { "type": "integer" } }, "required": ["Id"] } ] }
+    },
+    "additionalProperties": false
+}
 
